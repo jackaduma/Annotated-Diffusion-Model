83427bb8b40681b98e288792d7f137e150de0515
jackaduma <jackaduma@gmail.com>
2023-06-14 20:00:01 +0800

    add network_helper

---
+++ network_helper.py
@@ -0,0 +1,62 @@
+#!python
+# -*- coding: utf-8 -*-
+# @author: Kun
+
+import math
+from inspect import isfunction
+from functools import partial
+
+import matplotlib.pyplot as plt
+from tqdm.auto import tqdm
+from einops import rearrange, reduce
+from einops.layers.torch import Rearrange
+
+import torch
+from torch import nn, einsum
+import torch.nn.functional as F
+
+##############################################################################
+
+def exists(x):
+    return x is not None
+
+def default(val, d):
+    if exists(val):
+        return val
+    return d() if isfunction(d) else d
+
+
+def num_to_groups(num, divisor):
+    groups = num // divisor
+    remainder = num % divisor
+    arr = [divisor] * groups
+    if remainder > 0:
+        arr.append(remainder)
+    return arr
+
+
+class Residual(nn.Module):
+    def __init__(self, fn):
+        super().__init__()
+        self.fn = fn
+
+    def forward(self, x, *args, **kwargs):
+        return self.fn(x, *args, **kwargs) + x
+
+
+def Upsample(dim, dim_out=None):
+    return nn.Sequential(
+        nn.Upsample(scale_factor=2, mode="nearest"),
+        nn.Conv2d(dim, default(dim_out, dim), 3, padding=1),
+    )
+
+
+def Downsample(dim, dim_out=None):
+    # No More Strided Convolutions or Pooling
+    return nn.Sequential(
+        Rearrange("b c (h p1) (w p2) -> b (c p1 p2) h w", p1=2, p2=2),
+        nn.Conv2d(dim * 4, default(dim_out, dim), 1),
+    )
+
+
+##############################################################################
