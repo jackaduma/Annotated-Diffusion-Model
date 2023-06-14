98507b1fee353156a12d82981fdfce63430dde02
jackaduma <jackaduma@gmail.com>
2023-06-14 20:00:24 +0800

    add position_embedding

---
+++ position_embedding.py
@@ -0,0 +1,38 @@
+#!python
+# -*- coding: utf-8 -*-
+# @author: Kun
+
+
+import math
+from inspect import isfunction
+from functools import partial
+
+import matplotlib.pyplot as plt
+from tqdm.auto import tqdm
+from einops import rearrange, reduce
+from einops.layers.torch import Rearrange
+
+import torch
+from torch import nn, einsum
+import torch.nn.functional as F
+
+
+###########################################################################################
+
+class SinusoidalPositionEmbeddings(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.dim = dim
+
+    def forward(self, time):
+        device = time.device
+        half_dim = self.dim // 2
+        embeddings = math.log(10000) / (half_dim - 1)
+        embeddings = torch.exp(torch.arange(half_dim, device=device) * -embeddings)
+        embeddings = time[:, None] * embeddings[None, :]
+        embeddings = torch.cat((embeddings.sin(), embeddings.cos()), dim=-1)
+        return embeddings
+    
+###########################################################################################
+
+
